a0f941adad60a3d8415302f5602aeb1e0e0bf7e6
efgalvao <solfieri@hotmail.com>
2020-06-20 19:59:45 -0300

    Model update.

--- central/api/models.py
+++ central/api/models.py
@@ -1,32 +1,55 @@
 from django.db import models
+from django.core import validators
 
 # Create your models here.
 
-class Genero(models.Model):
-    genero = models.CharField("Genêro", max_length= 20)
-    teste = models.CharField("teste", max_length=5, default="teste")
+
+class User(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    last_login = models.DateTimeField('Último login', auto_now_add=True)
+    email = models.EmailField('E-mail', max_length=254, unique=True)
+    password = models.CharField('Senha', max_length=50, validators=[validators.MinLengthValidator(8)])
 
     def __str__(self):
-        return self.genero
+        return self.name
 
-class Autor(models.Model):
-    nome = models.CharField("Nome", max_length=100)
-
-    def __str__(self):
-        return self.nome
-
-class Livro(models.Model):
-    titulo = models.CharField("Título", max_length=150)
-    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
-    paginas = models.IntegerField("Páginas")
-    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
-    
-    class Meta:
-        ordering = ["titulo"]
+class Agent(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    status = models.BooleanField()
+    env = models.CharField(max_length=20)
+    version = models.CharField('Version', max_length=5)
+    address = models.GenericIPAddressField('Origem', protocol="IPV4", default="0.0.0.0")
 
     def __str__(self):
-        return self.titulo
+        return self.name
 
+class Group(models.Model):
+    name = models.CharField('Nome', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    ERROR_LEVEL = [
+        ('CRITICAL', 'CRITICAL'),
+        ('DEBUG', 'DEBUG'),
+        ('ERROR', 'ERROR'),
+        ('WARNING', 'WARNING'),
+        ('INFO', 'INFO'),
+    ]
+    level = models.CharField(max_length=20, choices=ERROR_LEVEL)
+    title = models.CharField(("Título"), max_length=50)
+    details = models.TextField("Dados")
+    teste = models.ForeignKey("api.Model", verbose_name=_(""), on_delete=models.CASCADE)
+    arquivado = models.BooleanField('Arquivado', default=False)
+    date = models.DateField('Data', auto_now=True)
+    agent_name = models.OneToOneField(Agent, verbose_name=_("agent_name"),on_delete=models.CASCADE)
+    agent_address = models.ForeignKey(Agent, verbose_name=_("agent_address"), on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+"""
 class Mae(models.Model):
     nome = models.CharField(max_length=10)
     teste = models.CharField(max_length=5)
@@ -34,3 +57,4 @@ class Mae(models.Model):
 class Filha(models.Model):
     nome = models.CharField(max_length=20)
     mae = models.ForeignKey(Mae, related_name="related", on_delete=models.CASCADE)
+"""

62207f7ead39b8f17528cea76081251794618767
efgalvao <solfieri@hotmail.com>
2020-06-24 14:15:51 -0300

    services updated

--- central/api/services.py
+++ central/api/services.py
@@ -15,5 +15,21 @@ def get_all_events_by_group(request, group):
     queryset = Event.objects.filter(agent__user__group=group)
     return queryset
 
-def order_events_by_level(request)
+def order_events_by_level(request):
     queryset = Event.objects.order_by('level')
+    return queryset
+
+def order_events_by_frequency():# Frequencia baseada em que ?
+    pass
+
+def search_events_for_level(request, campo):
+    queryset = Event.objects.filter(level__search=campo)
+    return queryset
+
+def search_events_for_description(request, campo):# data é diferente de description
+    queryset = Event.objects.filter(data__search=campo)
+    return queryset
+
+def search_events_for_address(request, campo):
+    queryset = Event.objects.filter(agent__address__search=campo)
+    return queryset

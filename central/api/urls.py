ad9db04ce49458281083e2e3ab4eac982fa863a4
efgalvao <solfieri@hotmail.com>
2020-06-27 23:24:20 -0300

    Events views

--- central/api/urls.py
+++ central/api/urls.py
@@ -1,22 +1,15 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import (UserAPI_objects, UserAPI_objects_details, AgentAPI_objects,
-                    AgentAPI_objects_details, GroupAPI_objects, GroupAPI_objects_details,
-                    EventAPI_objects, EventPI_objects_details, Event_list)
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import EventAPI_objects, EventAPI_objects_details, Event_list
 from .import services
 
 
 urlpatterns = [
-        path('',)
-        path('user/', UserAPI_objects.as_view()),
-        path('user/<int:pk>/', UserAPI_objects_details.as_view()),
-        path('agent/', AgentAPI_objects.as_view()),
-        path('agent/<int:pk>/', AgentAPI_objects_details.as_view()),
-        path('group/', GroupAPI_objects.as_view()),
-        path('group/<int:pk>/', GroupAPI_objects_details.as_view()),
         path('event/', EventAPI_objects.as_view()),
-        path('event/<int:pk>/', EventPI_objects_details.as_view()),
+        path('event/<int:pk>/', EventAPI_objects_details.as_view()),
         path('event/list/', Event_list.as_view(), name="events_list"),
+        path('get_token', obtain_auth_token)
         #path('register/', services.user_register, name='user_register')
        
         ]
--- central/api/views.py
+++ central/api/views.py
@@ -1,49 +1,39 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import User, Agent, Group, Event
-from .serializers import UserSerializer, AgentSerializer
-from .serializers import GroupSerializer, EventSerializer
+from rest_framework.views import APIView
+from .models import Event
+from .serializers import EventSerializer
 from .services import get_all_events
 from django.views.generic.list import ListView
 from django.views import View
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 # Create your views here.
 
 # Sign Up View
 
-class UserAPI_objects(generics.ListCreateAPIView):
-        queryset = User.objects.all()
-        serializer_class = UserSerializer
-
-class UserAPI_objects_details(generics.RetrieveUpdateDestroyAPIView):
-        queryset = User.objects.all()
-        serializer_class = UserSerializer
-
-class AgentAPI_objects(generics.ListCreateAPIView):
-        queryset = Agent.objects.all()
-        serializer_class = AgentSerializer
-
-class AgentAPI_objects_details(generics.RetrieveUpdateDestroyAPIView):
-        queryset = Agent.objects.all()
-        serializer_class = AgentSerializer
-
-class GroupAPI_objects(generics.ListCreateAPIView):
-        queryset = Group.objects.all()
-        serializer_class = GroupSerializer
-        
-class GroupAPI_objects_details(generics.RetrieveUpdateDestroyAPIView):
-        queryset = Group.objects.all()
-        serializer_class = GroupSerializer
-
 class EventAPI_objects(generics.ListCreateAPIView):
-        queryset = Event.objects.all()
-        serializer_class = EventSerializer
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+   #authentication_classes = [TokenAuthentication]
+    #permission_classes = [IsAuthenticated]
+    #def get(self, request, format=None):
+    """
+    Return a list of all users.
+    """
+        #events = [Event.data for Event in Event.objects.all()]
+        #return Response(events)
 
-class EventPI_objects_details(generics.RetrieveUpdateDestroyAPIView):
-        queryset = Event.objects.all()
-        serializer_class = EventSerializer
+class EventAPI_objects_details(generics.RetrieveAPIView):
+    #authentication_classes = [TokenAuthentication]
+    #permission_classes = [IsAuthenticated]
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
 
 class Event_list(View):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     def get(self, request):
         events = get_all_events()
         return render(request, 'api/event_list.html', {'events':events})

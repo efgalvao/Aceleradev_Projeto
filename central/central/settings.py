df8bfc273bb0f5a2105fca49b884d7512bffd7c3
efgalvao <solfieri@hotmail.com>
2020-06-29 15:51:21 -0300

    Settings update

--- central/central/settings.py
+++ central/central/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'api',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -57,8 +60,7 @@ ROOT_URLCONF = 'central.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ 'api/templates/' ], 
-        #[os.path.join(BASE_DIR, 'templates').replace('\\','/'),],
+        'DIRS': [TEMPLATE_DIR], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,4 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-AUTHENTICATION_BACKENDS = ('api.modelbackend.EmailBackend',)
+AUTH_USER_MODEL = 'users.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
